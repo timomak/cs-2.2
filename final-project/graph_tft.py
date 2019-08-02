081f2d2d1c33d9271913365f952d8b91d7ff91dc
timo <tmakhlay2@gmail.com>
2019-08-02 13:18:08 -0700

    Working graph rn

--- final-project/graph_tft.py
+++ final-project/graph_tft.py
@@ -37,7 +37,6 @@ class Champion(object):
         if len(self.class1) > 1:
             self.class2 = self.class1[1].lower()
             self.class1 = self.class1[0].lower()
-            print(self.class2, self.class1)
             self.classes + [self.class2, self.class1]
 
         else:
@@ -56,14 +55,50 @@ def main(filename):
     Makes the world go round.
     Runtime: O(n)
     """
+    champions = create_champ_obj(filename)
+    print("Number of champs:",len(champions))
+
+
+
+def create_champ_obj(filename):
+    """
+    Using the JSON file and the champions class, make a list containing all the champions.
+    """
     champions = []
     with open(filename) as json_file: # Open JSON File
         data = json.load(json_file) # Get the JSON
+        count = 0
         for champ_obj in data.items(): # O(n) ('aatrox' : { **aatrox data** } )
             new_champ = Champion(champ_obj[1])
             champions.append(new_champ)
+            count += 1
+    return champions
 
-    print(champions)
+# def _classes_dict(filename):
+#     """
+#     Open JSON file and read the data for the Classes (and Origins).
+#     filename - the file name as a string.
+#     Runtime: O(n)
+#     """
+#     class_dict = {} # {'robot': ['blitzcrank']}
+#     class_bonus_dict = {}
+#     dict = { 1: {}, 2: {}, 3: {}, 4 : {}, 6 : {}} # { 1 : { 'robot' : set['blitzcrank'], 'exile' : set['yasuo'] }, 2 : ... }
+#     with open(filename) as json_file:
+#         data = json.load(json_file)
+#         for class_obj in data.items(): # O(n)
+#
+#             key = class_obj[1]['key'] # String
+#             name = class_obj[1]['name'] # String
+#             description = class_obj[1]['description'] # String
+#             accentChampionImage = class_obj[1]['accentChampionImage'] # URL as String
+#             bonuses = class_obj[1]['bonuses'] # Array [{'needed': int, 'effect': string}]
+#             needed = bonuses[-1]['needed'] # Check the highest number for needed. (In this case it's the last item in the array)
+#
+#             class_dict[key] = []
+#             class_bonus_dict[key] = needed
+#             dict[needed].update({class_obj[0]: []})
+#
+#     return dict
 
 if __name__ == '__main__':
     main('champions.json')
